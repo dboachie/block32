0a7ee48e59c3d3bfbd1a3cb6ef798fcc25b5612a
Daniel Boachie <dboachie222@gmail.com>
2024-11-11 20:53:14 -0500

    Acme Ice Cream Shop

--- index.js
+++ index.js
@@ -1,25 +1,123 @@
 const pg = require("pg");
 const express = require("express");
 const { Client } = require("pg");
+const { WatchOutlined } = require("@mui/icons-material");
 const client = new Client(
   "postgresql://dkb:Avt5000*@localhost:5432/the_acme_notes_db"
 );
+client.connect();
 const app = express();
 app.use(express.json());
 
 app.get("/api/get-all-flavors", async (req, res, next) => {
-  await client.connect();
+
   try {
     const flavors = await client.query(`
       SELECT * FROM flavors;`);
 
     res.status(201).json(flavors);
-    await client.end();
+    
   } catch (err) {
     console.error("Couldnt fetch flavors", err);
-    await client.end();
+    
     res.status(401).send({ messsage: "couldnt retrieve" });
-  }
+  } 
+});
+
+app.get("/api/get-all-flavors/:id", async (req, res, next) => {
+
+  try {
+    const SQL = `
+      SELECT * FROM flavors WHERE id=$1 
+      
+    `;
+    // SELECT means return
+    const response = await client.query(SQL, [req.params.id]);
+    res.send(response.rows); 
+    
+  } catch (err) {
+    next(err);
+  } 
+});
+
+app.post("/api/get-all-flavors", async (req, res, next) => {
+ const { name, is_fav } = req.body;
+
+  try {
+    const response = await client.query(`
+    INSERT INTO flavors (name, is_favorite)
+    VALUES ($1, $2)
+    RETURNING *
+    ;`, [name, is_fav]);
+
+    res.send(response.rows[0]);
+    
+  } catch (err) {
+    next(err);
+  } 
+
+
+
+
+});
+
+// app.post("/api/get-all-flavors", async (req, res, next) => {
+//   const {name, is_fav} = req.body;
+
+// try {
+//   const SQL = `
+//     INSERT INTO flavors(name, is_favorite)
+//     VALUES($1, $2)
+//     RETURNING *
+//   `
+//   const response = await client.query(SQL, [name, is_fav]);
+//   res.send(response.rows[0])
+// } catch (err) {
+//   next(err)
+// }
+
+// });
+
+
+
+
+
+
+
+app.delete("/api/get-all-flavors/:id", async (req, res, next) => {
+
+  try {
+    const SQL = `
+      DELETE FROM flavors
+      WHERE id = $1
+    `;
+    const response = await client.query(SQL, [req.params.id]);
+    res.sendStatus(204);
+  } catch (err) {
+    next(err);
+  } 
+
+});
+
+app.put("/api/get-all-flavors/:id", async (req, res, next) => {
+
+  try {
+    const SQL = `
+      UPDATE flavors
+      SET id=$1, name=$2, is_favorite=$3
+      WHERE id=$1 RETURNING *
+    `;
+    const response = await client.query(SQL, [
+
+      req.params.id,
+      req.body.name,
+      req.body.is_favorite,
+      
+    ]);
+    res.send(response.rows[0]);
+  } catch (err) {
+    next(err);
+  } 
 });
 
 app.listen(3000, () => console.log("listening"));
